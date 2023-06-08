904ecbed6a97983355d37fc09b08d513c94b2bdc
Mohamed El Yakoubi <102755988+Mohamed12elyakoubi@users.noreply.github.com>
2023-06-08 21:20:21 +0200

    Add files via upload

---
+++ Palindroom.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Voer een woord in:");
+        string woord = Console.ReadLine();
+
+        if (IsPalindroom(woord))
+        {
+            Console.WriteLine("Het woord is een palindroom.");
+        }
+        else
+        {
+            Console.WriteLine("Het woord is geen palindroom.");
+        }
+    }
+
+    static bool IsPalindroom(string woord)
+    {
+        int lengte = woord.Length;
+
+        for (int i = 0; i < lengte / 2; i++)
+        {
+            if (woord[i] != woord[lengte - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
