5b3a8c3aa0db5017b1546a26ccd1b3d8a263643f
Mohamed El Yakoubi <102755988+Mohamed12elyakoubi@users.noreply.github.com>
2023-06-08 21:47:14 +0200

    Add files via upload

---
+++ Postcode.cs
@@ -0,0 +1,42 @@
+﻿
+Console.WriteLine("Voer een postcode in:");
+    string postcode = Console.ReadLine();
+
+    postcode = postcode.Replace(" ", "").ToUpper();
+        if (postcode.Length == 6)
+        {
+            bool firstFournumbers = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (!Char.IsDigit(postcode[i]))
+                {
+                    firstFournumbers= false;
+                    break;
+                }
+            }
+
+            // Controleer of de laatste 2 tekens letters zijn
+            bool lastTwoLetters = true;
+            for (int i = 4; i < 6; i++)
+            {
+                if (!Char.IsLetter(postcode[i]))
+                {
+                    lastTwoLetters = false;
+                    break;
+                }
+            }
+
+            // Geef het resultaat weer
+            if (firstFournumbers && lastTwoLetters)
+            {
+                Console.WriteLine("De postcode is correct.");
+            }
+            else
+            {
+                Console.WriteLine("De postcode is onjuist.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("De postcode is onjuist.");
+        }
