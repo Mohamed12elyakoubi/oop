bbdec52e2596978ea94f32ff2fe236e95e526270
Mohamed El Yakoubi <102755988+Mohamed12elyakoubi@users.noreply.github.com>
2023-06-08 22:48:54 +0200

    Pi opdracht

---
+++ Pi.cs
@@ -0,0 +1,15 @@
+﻿
+var n = 100000000;
+var r = 0.0;
+var plus = true;
+
+for (var i = 1; i <n; i += 2)
+{
+    var fraction = 1.0 / i;
+    r += plus ? fraction : -fraction;
+    plus = !plus;
+}
+
+Console.WriteLine(r*4);
+Console.WriteLine(Math.PI);
+
